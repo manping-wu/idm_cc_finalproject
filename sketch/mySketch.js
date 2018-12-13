e371ce1cfe2a2564cd5cb63ec8a78326db17aa51
Man-Ping Wu <mpamywu@gmail.com>
2018-12-12 21:29:34 -0500

    finished project

--- sketch/mySketch.js
+++ sketch/mySketch.js
@@ -1,3 +1,23 @@
+
+//---> Concept
+
+/*
+Title: Social Detox
+
+Concept:
+This piece is about people's social media addiction regarding notifications. This project is both an interactive 
+and time based piece, where users test their limits by not clicking on the notifications in real time.
+It's a persuasive piece to encourage users not to rely on the notifications to gain temporary satifisation, 
+but using their time wisely to do something productive. 
+
+
+*/
+
+
+
+
+//----> Blueprint of the Final Project
+
 /*
 
 Time as a health bar
@@ -57,6 +77,11 @@ everytime the notifications are clicked:
 */
 
 
+//phrase 2 
+//add music 
+//add background effects
+
+
 //---> global variables
 
 //Healthbar&Timer variables
@@ -79,15 +104,17 @@ intial timer at what seconds should it be
 57,600 seconds is 960
 */
 
-
 var maxHealth = 500; //max length of the timer/health bar 
 var healthLength = 500; //full length of the timer/health bar 
 var countDown; // countdown variable
 var consumedTime =0; //intial at 0
 var s ; //to map out the variable between real time countdown and the length of the timer/health bars
+var lastSecond = 0 //down to the last second
+var secondPassCount = 0 // the next coming second
+
 
 //Notification Variable 
-var popUp;
+var popUps = [];
 var email;
 var insta;
 var snapchat;
@@ -97,51 +124,48 @@ var imessage;
 var slack;
 var twitter;
 
+//mode variable
+var mode = 1; //intial value of the mode is 1 and will switch between them.
 
+//Emoji Variable 
+var sillyFace;
+var tongueOut;
 
-
-//phrase 1
-//goal 1: figure out the alogrithm to generate notifications and when clicked on it will indepdently decrease the time
-//goal 2: how to add up the times
-//make the backgrounds for the beginning and ending
-
-//phrase 2 
-//add music 
-//add background effects
-
-
+//font variables
+var rubikR //rubik regular
+var rubikB //rubik bold
 
 
 //----->Preload Function
 function preload (){
 
+	//font preload
+	rubikR = loadFont ('asset/fonts/Rubik-Regular.ttf')
+	rubikB = loadFont ('asset/fonts/Rubik-Bold.ttf')
 
 
 	//notification image upload
-	email = loadAnimation ('asset/email/email_01.png','email_20.png')
-	email.looping = false;
+	email = loadAnimation ('asset/email/email_01.png','asset/email/email_20.png')
 
-	insta = loadAnimation ('asset/instagram_01.png', 'asset/instagram_013.png')
-	insta.looping = false;
+	insta = loadAnimation ('asset/instagram/instagram_01.png', 'asset/instagram/instagram_13.png')
 	
-	snapchat = loadAnimation ('snapchat_01.png', 'snapchat_09.png')
-	snapchat.looping = false;
+	snapchat = loadAnimation ('asset/snapchat/snapchat_01.png', 'asset/snapchat/snapchat_09.png')
 	
-	facebook = loadAnimation ('facebook_01.png', 'facebook_07.png')
-	facebook.looping = false;
+	facebook = loadAnimation ('asset/facebook/facebook_01.png', 'asset/facebook/facebook_07.png')
 	
-	facetime = loadAnimation ('facetime_01.png', 'facetime_05.png')
-	facetime.looping = false;
+	facetime = loadAnimation ('asset/facetime/facetime_01.png', 'asset/facetime/facetime_05.png')
 	
-	imessage = loadAnimation ('imessage_01.png', 'imessage_05.png')
-	imessage.looping = false;
+	imessage = loadAnimation ('asset/imessage/imessage_01.png', 'asset/imessage/imessage_05.png')
 	
-	slack = loadAnimation ('slack_01.png', 'slack_05.png')
-	slack.looping = false;
+	slack = loadAnimation ('asset/slack/slack_01.png', 'asset/slack/slack_05.png')
 	
-	twitter = loadAnimation ('twitter_01.png', 'twitter_04.png')
-	twitter.looping = false;
+	twitter = loadAnimation ('asset/twitter/twitter_01.png', 'asset/twitter/twitter_04.png')
 
+
+	//emoji upload
+	sillyFace = loadImage ('asset/emoji/upside-down.png')
+	tongueOut = loadImage ('asset/emoji/tongue_out.png')
+	
 
 }
 
@@ -151,129 +175,251 @@ function setup() {
 	createCanvas(windowWidth, windowHeight); // size of the window
 	background(255); //background color
 
-
 	//arrays for the values for the notifications
 	var slide = [19, 12, 8, 6, 4, 4, 4, 3];
-	var iconX = [1200, 200, 400, 500, 900, 900, 100, 800]; 
-	var iconY = [500, 200, 400, 100, 300, 600, 500, 100];
+	var iconX = [650, 150, 900, 900, 400, 150, 400, 650]; 
+	var iconY = [200, 450, 450, 200, 200, 200, 450, 450];
+	var labels = ["email", "insta", "snapchat", "facebook", "facetime", "imessage", "slack", "twitter"]
 	var icon = [email, insta, snapchat, facebook, facetime, imessage, slack, twitter];
+	var unitConsumed = [10, 11, 10, 15, 5, 12, 10, 5];
+	var freq = [65, 80, 50, 220, 65, 20, 70, 90];
+	
+// (labelTemp, iconTemp, slideTemp, iconXTemp, iconYTemp, unitCounsumeTemp)
 
 
+	for (var i = 0; i< slide.length; i++){
 
-	for (var i = 0; i<slide.length[i]; i++){
-
-		popUp = new Notification (icon[i],slide[i],iconX[i], iconY[i])
+		popUps.push( new Notification (labels[i],icon[i],slide[i],iconX[i], iconY[i], unitConsumed[i], freq[i]))
 
 
 	}
+	console.log(popUps[0])
+
+
 
 
 
 }
+
+//----->Labelling the Notifications Function
+function getNotibyLabel(label){
+
+//for looping the popUps arrays the popups equals to the labels from the array
+	for (var i = 0; i<popUps; i++){
+		if (popUps[i].label==label){
+			return popUps[i]
+		}
+
+	}
+}
+
 
 
 //----->MousePressed Function
 function mousePressed(){
-	consumedTime-=10*60
+
+	if (mode==2){
+		//for looping the popUps arrays with actual notifications in the corner to mousePressed on within the notification image
+		for (var i = 0; i<popUps.length; i++){
+			consumedTime+=popUps[i].handleClick();
+
+		}
+	}
+	
+	if (mode==1){
+		mode=2 //switch to mode 2 when mousePressed
+	}
+
+	if (mode==3){
+		consumedTime=0
+		mode=1 //switch to mode 2 when mousePressed
+	}
 }
+
+
+
+
 
 
 //----->Draw Function
 function draw() {
-	background(255)
 
-	healthTimer(); //health timer function added
+	//mode 1 is the introduction
+	if (mode==1){
+		beginning();
+
+	}
+
+	//mode 2 is the game
+	if(mode==2){
+		
+
+		background(255)
+		// 	getNotibyLabel('email').update()
+		// 	getNotibyLabel('insta').update()
+		// 	getNotibyLabel('snapchat').update()
+		// 	getNotibyLabel('facebook').update()
+		// 	getNotibyLabel('facetime').update()
+		// 	getNotibyLabel('imessage').update()
+		// 	getNotibyLabel('slack').update()
+		// 	getNotibyLabel('twitter').update()
+			console.log(consumedTime);
+			
+			// if (popUps[0]){
+				popUps[0].display()
+				popUps[1].display()
+				popUps[2].display()
+				popUps[3].display()
+				popUps[4].display()
+				popUps[5].display()
+				popUps[6].display()
+				popUps[7].display()
+			// }
+				
+				healthTimer(); //health timer function added
+		
+		
+				var nowSecond = second()
+		
+				if (nowSecond!=lastSecond){
+					secondPassCount++
+		
+		
+				}
+		
+				lastSecond = second()
 
 
+
+	}
+
+	//mode 3 is the end
+	if (mode==3){
+		ending();
+
+	}
 
 }
 
 
 //-----> Custom Function 
-//-----> Health Timer
+//Custom Health Timer
 function healthTimer(){
-
-
-
-	// variable about having the time count down in real time
-	// (hour (24) subtract 10 to get to 14 hours) multiply 60 to get minutes then multiply 60 to get seconds follow by minutes and seconds
-	s= (hour()-10)*60*60+minute()*60+second();
 	
-	// if the hour is less than 0, the variable will be 0
-	if (hour()-10<0){
-		s=0
-		ending();
-		//ending display
-		//how to connect notifications class with the timer
-
-
+	
+	
+		//healthbar
+	
+		//TEXT 
+		textFont(rubikB)
+		fill(150)
+		textSize (15);
+		text ('Time Remaining', 50,40);
+	
+	
+	
+		//map out the full 57,600 seconds into the full length of the healthbar
+		var countDown = map(s,57600,0,0,500);
+	
+	
+		//countDown over the maxHealth multiply by the lenght of the healthbar
+		var healthInProcess = (countDown/maxHealth)*healthLength;
+	
+	
+		var emptyHealth = countDown - consumedTime
+		if(emptyHealth<=0){
+			mode=3
+		}
+	
+		//the boundary of the timer 
+	
+		stroke(190);
+		noFill();
+		rect(50,50,500,30); //1. how to stop the boundary from moving sideways.
+	
+	
+		//the inside of the timer
+		fill(45,255,111);
+		// rect(50,50,emptyHealth,30);
+		
+		if (emptyHealth >0) {
+			
+		rect(50,50,emptyHealth,30);	
+			
+		}
+		
+		if(emptyHealth = 0){
+			
+		fill(255)
+			
+		}
+	
+	
+		
+	
+		// variable about having the time count down in real time
+		// (hour (24) subtract 10 to get to 14 hours) multiply 60 to get minutes then multiply 60 to get seconds follow by minutes and seconds
+		s= (hour()-10)*60*60+minute()*60+second();
+		
+		// if the hour is less than 0, the variable will be 0
+		if (hour()-10<0){
+			s=0
+			ending();
+			//ending display
+			//how to connect notifications class with the timer
+	
+			
+	
+		}
+	
+		//s variable will be affected by the time consumed going down
+		s-=consumedTime
+		// s=frameCount*100
+		// console.log(s)
+	
+	
+	
 	}
+//-----> Opening and Ending
 
-	//s variable will be affected by the time consumed going down
-	s-=consumedTime
-	// s=frameCount*100
-	console.log(s)
-
-	//healthbar
-
-	//TEXT 
-	textSize (15);
-	text ('Time Remaining', 50,40);
-
-
-
-	//map out the full 57,600 seconds into the full length of the healthbar
-	var countDown = map(s,57600,0,0,500);
-
-
-	//countDown over the maxHealth multiply by the lenght of the healthbar
-	var healthInProcess = (countDown/maxHealth)*healthLength;
-
-
-
-	//the boundary of the timer 
-	stroke(2);
-	noFill();
-	rect(50,50,500,30);
-
-
-	//the inside of the timer
-	fill(0,255,0);
-	rect(50,50,countDown,30);
-
-
-
-
-
-}
-
-
+//custom beginning function (mode 1)
 function beginning(){
 
-	
-	background(255,195,254)
-	textSize(20)
-	text ('Try Not to Click on the Notifications', windowWidth/2, windowHeight/2)
+	background(245,172,255) //background of mode 1
 
+	image (sillyFace,windowWidth/2-20 ,200) //emoji face
+	textFont (rubikR)
+	fill(255) //white text
+	textAlign(CENTER) //center the text
+	textSize(60)
+	text ('WARNING',windowWidth/2+23,windowHeight/2-20)
+	textSize(20) //text size
+	textAlign(CENTER) //center the text
+	text ('Do Not Attempt to Click on the Notifications', windowWidth/2+30, windowHeight/2+15)
+	//placement of the text
 
 }
 
-function mouseClicked(){
 
-
-		beginning() == false
-	
-
-}
-
+//custom ending function (mode 3)
 function ending (){
 
-	var conclusion = consumedTime 'minutes could had been spent on reading books instead of social media'
- 	background(255,195,254)
-	textSize(20)
-	text(conclusion,windowWidth/2,windowHeight/2)
+	background(189,228,255) //background of mode 1
 
-
+	//variable for the text 
+	var conclusion = 'could had been spent reading books instead of social media'
+	 image (tongueOut,windowWidth/2-20 ,200) 
+	 	//don't need to repeat the styles as it will cascade down to the next text
+	 fill(255) //white text
+	 textAlign(CENTER) //center the text
+	 textSize(120)
+	 textFont(rubikB)
+	 text (consumedTime+ ' Minutes',windowWidth/2+20,windowHeight/2+10) //text and calulated time
+	 textFont(rubikR)
+		
+	textSize(20) //text size
+	text (conclusion, windowWidth/2, windowHeight/1.8)
+	//placement of the text
 }
 
 
--- sketch/notification.js
+++ sketch/notification.js
@@ -1,84 +1,91 @@
-class notification{
+class Notification{
+	constructor(labelTemp, iconTemp, slideTemp, iconXTemp, iconYTemp, unitCounsumeTemp, freqTemp){
+
+			this.label = labelTemp;
+			this.icon = iconTemp;
+			this.slide = slideTemp;
+			this.iconX = iconXTemp;
+			this.iconY = iconYTemp;
+		  this.unitConsume = unitCounsumeTemp;
+			this.clicked=false
+			this.freq = freqTemp;
+
+			this.counter = int(random(1,35));
+
+
+			// this.icon = loadAnimation ()
+			this.icon.looping = false;
+
+		}
+	display(){
+		if (this.clicked){
+			if (frameCount % this.freq==0){
+				this.counter++
+			}
+		}
+		push()
+			var generatedStarter = random (1,35) 
+
+			textFont(rubikR)
+			fill (255)
+			text(generatedStarter,this.iconX,this.iconY)
+			fill (255,0,0)
+
+
+			if (this.icon.getFrame() == this.icon.getLastFrame()){
+				this.icon.changeFrame (this.slide);
+			}
+
+			translate(this.iconX, this.iconY)
+			
+			var w = popUps[0].icon.getWidth()
+			var h = popUps[0].icon.getHeight()
+			animation (this.icon, w/2,h/2);
+
+		
+			noStroke()
+			fill(255,76,71)
+			ellipse(170, 15, 40,40)
+			fill(255)
+			textSize(20)
+			text(this.counter, 160,23);
+
+
+		
+		pop()
+		
 
 
 
-constructor(iconTemp, slideTemp, iconXTemp, iconYTemp){
 
-this.icon = iconTemp;
-this.slide = slideTemp;
-this.iconX = iconXTemp;
-this.iconY = iconYTemp;
-
-
-this.icon = loadAnimation ()
-this.icon.looping = false;
-
-}
-
-
-
-}
-
-
-//HELP HERE
-update(){
-
-var generatedStarter = random (1,35) 
-var sec3 = second () //how to just place 3 seconds
-var startingPoint = random (2,5) second() // start either 2-5 seconds. 
-var numberOfClicks
-
-
-fill (255)
-text(generatedStarter,icon.x-192,icon.y)
-fill (255,0,0)
-ellipse(icon.x-190, icon.y, 30,30)
-
-
-
-
-//need some help with time
-//every 3 seconds build up to the max notification number
-if (sec3) {
-
-	generatedStarter+=1; 
-}
-
-
-if (mouseIsPressed) {
-
-	//somehow notifications dissapear
-	//add up the consumed time notifications here
-	numberOfClicks.add(generatedStarter)
+	}
+	handleClick(){
+		console.log("handle");
+		var w = popUps[0].icon.getWidth()
+		var h = popUps[0].icon.getHeight()
+		
+		if (mouseX> this.iconX && mouseX<this.iconX+w &&
+				mouseY> this.iconY && mouseY<this.iconY+h){
+			console.log(this.label)
+			this.icon.rewind();
+			var result = this.unitConsume*this.counter
+			this.counter=0
+			this.clicked=true
+			
+			
+			
+			
+			return result
+			
+			
+			
+		}
+		
+		return 0
+		
 	
-
-}
-
-
-
-}
-
-
-display(){
-
-if (this.icon.getFrame() == this.icon.getLastFrame())
-	this.icon.changeFrame (this.slide);
-
-animation (this.icon, this.iconX, this.iconY);
-
-
-	
-	
-
-if (mouseIsPressed){
-
-
-
-	this.icon.rewind();
-
-
-}
-
-
-
+		
+		
+		
+	}
 }
